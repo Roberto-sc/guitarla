e54932ddc5a3d7235514efeb7cfdebc897100642
Roberto-sc <luis.solano@ucrso.info>
2024-03-21 18:39:43 -0600

    Commit First Version

--- src/App.jsx
+++ src/App.jsx
@@ -1,55 +1,107 @@
-import { useEffect, useState } from "react"
-import Guitar from "./components/Guitar"
-import Header from "./components/Header"
-import { db } from "./data/db"
-
+import { useEffect, useState } from "react";
+import Guitar from "./components/Guitar";
+import Header from "./components/Header";
+import { db } from "./data/db";
 
 function App() {
- 
-  const [data,setData] = useState(db)
-  const [cart,setCart] = useState([])
 
-  function addToCart (item) {
 
-    const itemExist = cart.findIndex(guitar=> guitar.id === item.id)
-    if(itemExist >= 0){
-      const updatedCart = [... cart]
-      updatedCart[itemExist].quantity++
-      setCart(updatedCart)
-    }else{
+  const initialCart = () => {
+    const localStorageCart = localStorage.getItem('cart')
+
+    return localStorageCart ? JSON.parse(localStorageCart) : []
+  }
+  const [data] = useState(db);
+  const [cart, setCart] = useState(initialCart);
+
+  const MAX_ITEMS = 5
+  const MIN_ITEMS = 1
+
+  useEffect(() => {
+    localStorage.setItem('cart',JSON.stringify(cart))
+  }
+  ,[cart])
+
+  function addToCart(item) {
+    const itemExist = cart.findIndex((guitar) => guitar.id === item.id);
+    if (itemExist >= 0) {
+      if(cart[itemExist].quantity >= MAX_ITEMS) return
+      const updatedCart = [...cart];
+      updatedCart[itemExist].quantity++;
+      setCart(updatedCart);
+    } else {
       item.quantity = 1;
-      setCart([...cart,item])
+      setCart([...cart, item]);
     }
+  }
 
-    
+  function removeCart(id) {
+    setCart(prevCart => prevCart.filter(guitar => guitar.id !== id))
+  }
+  
+  function increaseQuantity(id){
+    const updatedCart = cart.map(item =>{
+      if(item.id === id && item.quantity < MAX_ITEMS){
+        return{
+          ...item,
+          quantity : item.quantity+1
+        }
+      }
+      return item
+    })
+    setCart(updatedCart)
+  }
+
+  function decreaseQuantity(id){
+    const updatedCart = cart.map(item => {
+      if(item.id === id && item.quantity>MIN_ITEMS){
+        return{
+          ...item,
+          quantity: item.quantity-1
+        }
+      }
+      return item
+    })
+    setCart(updatedCart)
+  }
+
+  function clearCart () {
+    setCart([])
   }
 
   return (
     <>
-    <Header/>
-    <main className="container-xl mt-5">
+      <Header 
+        removeCart={removeCart} 
+        cart={cart} 
+        increaseQuantity={increaseQuantity} 
+        decreaseQuantity={decreaseQuantity} 
+        clearCart={clearCart}
+      />
+      <main className="container-xl mt-5">
         <h2 className="text-center">Nuestra Colección</h2>
 
-        <div className="row mt-5">          
-            {data.map(guitar =>(
-              <Guitar
-                key={guitar.id}
-                guitar={guitar}
-                addToCart={addToCart}
-                setCart={setCart}
-              />
-            ))}
+        <div className="row mt-5">
+          {data.map((guitar) => (
+            <Guitar
+              key={guitar.id}
+              guitar={guitar}
+              addToCart={addToCart}
+              setCart={setCart}
+            />
+          ))}
         </div>
-    </main>
+      </main>
 
-
-    <footer className="bg-dark mt-5 py-5">
+      <footer className="bg-dark mt-5 py-5">
         <div className="container-xl">
-            <p className="text-white text-center fs-4 mt-4 m-md-0">GuitarLA - Todos los derechos Reservados</p>
+          <p className="text-white text-center fs-4 mt-4 m-md-0">
+            GuitarLA - Todos los derechos Reservados
+          </p>
         </div>
-    </footer>
+      </footer>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
